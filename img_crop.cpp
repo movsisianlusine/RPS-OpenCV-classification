8bc91e41a80a377cd0cade7050115ac85d5955b6
lusine <movsisianlusine@gmail.com>
2026-03-10 22:20:19 +0400

    Changed the code to use polymorphism. Created an abstract class that has 2 derived classes: one for 'simple' directories and one for 'complex' ones.

--- img_crop.cpp
+++ img_crop.cpp
@@ -6,10 +6,11 @@
 #include <vector>
 #include <filesystem>
 #include <algorithm>
+#include "img_crop.h"
 
 namespace fs = std::filesystem;
 
-std::vector<bool> get_contains_obj(const cv::Mat& img) {
+std::vector<bool> ImageCropper::get_contains_obj(const cv::Mat& img) {
     const int height = img.rows;
     const int white_threshold = 255;
     std::vector<bool> contains_obj(height, false);
@@ -29,61 +30,126 @@ std::vector<bool> get_contains_obj(const cv::Mat& img) {
     return contains_obj;
 }
 
-void img_crop(const std::string& img_path, const fs::path& cropped_save_path) {
-    if (fs::exists(cropped_save_path)) {
-        fs::remove_all(cropped_save_path);
+
+
+void SimpleImageCropper::img_crop(const std::string& img_path, const fs::path& cropped_save_path) {
+    if (!cropped_save_path.empty()) {
+        for (const auto& dir : fs::directory_iterator(cropped_save_path)) {
+            fs::remove_all(dir.path());
+        }
     }
-    fs::create_directories(cropped_save_path);
 
     cv::Mat img(cv::imread(img_path));
     if (img.empty()) {
-        std::cerr << "Error: Could not open or find the image at " << img_path << std::endl;
-        return;
+        throw std::runtime_error("Could not open or find the image at " + img_path);
     }
 
-    cv::Mat left_half_img = img(cv::Rect(0, 0, img.cols / 2, img.rows));
-    cv::Mat right_half_img = img(cv::Rect(img.cols / 2, 0, img.cols / 2, img.rows));
+    int height = img.rows;
+    int width = img.cols;
 
-    const std::vector<bool> contains_obj_left = get_contains_obj(left_half_img);
-    const std::vector<bool> contains_obj_right = get_contains_obj(right_half_img);
+    std::vector<bool> containsObj = get_contains_obj(img);
 
-    int start_y_left = 0;
-    int start_y_right = 0;
-    bool in_obj_left = false;
-    bool in_obj_right = false;
-    int round_left = 1;
-    int round_right = 1;
-    const int min_height = 5;
+    int startY = 0;
+    int endY = 0;
+    bool inObj = false;
+    int round = 1;
 
-    for (int y = 0; y < img.rows; y++) {
-        if (contains_obj_left[y] && !in_obj_left) {
-            start_y_left = y;
-            in_obj_left = true;
+    constexpr const int MIN_OBJ_HEIGHT = 8;
+    constexpr const char* LEFT_PREFIX = "left_half_";
+    constexpr const char* RIGHT_PREFIX = "right_half_";
+    constexpr const char* EXT = ".png";
+
+    for (int y = 0; y < height; y++) {
+        if (containsObj[y] && !inObj) {
+            startY = y;
+            inObj = true;
         }
-        if ((!contains_obj_left[y] || y == img.rows - 1) && in_obj_left) {
-            int h = y - start_y_left;
-            if (h >= min_height) {
-                cv::Rect left_sign(0, start_y_left, img.cols / 2, h);
-                std::string save_path = (cropped_save_path / ("left_half_" + std::to_string(round_left) + ".png")).string();
-                cv::imwrite(save_path, img(left_sign));
-                round_left++;
+
+        if ((!containsObj[y] || y == height - 1) && inObj) {
+            endY = y;
+
+            if (endY - startY < MIN_OBJ_HEIGHT) {
+                inObj = false;
+                continue;
             }
-            in_obj_left = false;
-        }
 
-        if (contains_obj_right[y] && !in_obj_right) {
-            start_y_right = y;
-            in_obj_right = true;
-        }
-        if ((!contains_obj_right[y] || y == img.rows - 1) && in_obj_right) {
-            int h = y - start_y_right;
-            if (y - start_y_right >= min_height) {
-                cv::Rect right_sign(img.cols / 2, start_y_right, img.cols / 2, y - start_y_right);
-                std::string save_path = (cropped_save_path / ("right_half_" + std::to_string(round_right) + ".png")).string();
-                cv::imwrite(save_path, img(right_sign));
-                round_right++;
-            }
-            in_obj_right = false;
+            cv::Rect left_sign(0, startY, width / 2, endY - startY);
+            cv::Rect right_sign(width / 2, startY, width / 2, endY - startY);
+
+            cv::Mat left_img = img(left_sign);
+            cv::Mat right_img = img(right_sign);
+
+            std::string left_half = std::string(LEFT_PREFIX) + std::to_string(round) + std::string(EXT);
+            std::string right_half = std::string(RIGHT_PREFIX) + std::to_string(round) + std::string(EXT);
+
+            fs::path left_path = cropped_save_path / left_half;
+            fs::path right_path = cropped_save_path / right_half;
+
+            cv::imwrite(left_path.string(), left_img);
+            cv::imwrite(right_path.string(), right_img);
+
+            round++;
+            inObj = false;
         }
     }
 }
+
+void ComplexImageCropper::img_crop(const std::string& img_path, const fs::path& cropped_save_path) {
+        if (fs::exists(cropped_save_path)) {
+            fs::remove_all(cropped_save_path);
+        }
+        fs::create_directories(cropped_save_path);
+
+        cv::Mat img(cv::imread(img_path));
+        if (img.empty()) {
+            throw std::runtime_error("Error: Could not open or find the image at " + img_path);
+        }
+
+        cv::Mat left_half_img = img(cv::Rect(0, 0, img.cols / 2, img.rows));
+        cv::Mat right_half_img = img(cv::Rect(img.cols / 2, 0, img.cols / 2, img.rows));
+
+        const std::vector<bool> contains_obj_left = get_contains_obj(left_half_img);
+        const std::vector<bool> contains_obj_right = get_contains_obj(right_half_img);
+
+        int start_y_left = 0;
+        int start_y_right = 0;
+        bool in_obj_left = false;
+        bool in_obj_right = false;
+        int round_left = 1;
+        int round_right = 1;
+        const int min_height = 5;
+
+        for (int y = 0; y < img.rows; y++) {
+            if (contains_obj_left[y] && !in_obj_left) {
+                start_y_left = y;
+                in_obj_left = true;
+            }
+            if ((!contains_obj_left[y] || y == img.rows - 1) && in_obj_left) {
+                int h = y - start_y_left;
+                if (h >= min_height) {
+                    cv::Rect left_sign(0, start_y_left, img.cols / 2, h);
+                    std::string save_path = (cropped_save_path / ("left_half_" + std::to_string(round_left) + ".png")).string();
+                    cv::imwrite(save_path, img(left_sign));
+                    round_left++;
+                }
+                in_obj_left = false;
+            }
+
+            if (contains_obj_right[y] && !in_obj_right) {
+                start_y_right = y;
+                in_obj_right = true;
+            }
+            if ((!contains_obj_right[y] || y == img.rows - 1) && in_obj_right) {
+                int h = y - start_y_right;
+                if (y - start_y_right >= min_height) {
+                    cv::Rect right_sign(img.cols / 2, start_y_right, img.cols / 2, y - start_y_right);
+                    std::string save_path = (cropped_save_path / ("right_half_" + std::to_string(round_right) + ".png")).string();
+                    cv::imwrite(save_path, img(right_sign));
+                    round_right++;
+                }
+                in_obj_right = false;
+            }
+        }
+    
+}
+
--- img_crop.h
+++ img_crop.h
@@ -1,7 +1,28 @@
 #pragma once
 #include <string>
 #include <filesystem>
+#include <vector>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/opencv.hpp>
+
 
 namespace fs = std::filesystem;
 
-void img_crop(const std::string& img_path, const fs::path& cropped_save_path);
+class ImageCropper {
+public:
+	virtual void img_crop(const std::string& img_path, const fs::path& cropped_save_path) = 0;
+	std::vector<bool> ImageCropper::get_contains_obj(const cv::Mat& img);
+	virtual ~ImageCropper() = default;
+};
+
+class SimpleImageCropper : public ImageCropper{
+public:
+	void img_crop(const std::string& img_path, const fs::path& cropped_save_path) override;
+};
+
+class ComplexImageCropper : public ImageCropper {
+public:
+	void img_crop(const std::string& img_path, const fs::path& cropped_save_dir) override;
+};
+
